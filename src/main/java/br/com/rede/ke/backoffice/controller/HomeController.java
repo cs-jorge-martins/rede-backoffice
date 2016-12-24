742b853b6d530bb2bfa0c178ac4c64d624cd5434
Johnny Richard <jrichard@thoughtworks.com>
2016-12-24 18:34:54 -0200

    Adiciona busca filtrada para PV

--- src/main/java/br/com/rede/ke/backoffice/controller/HomeController.java
+++ src/main/java/br/com/rede/ke/backoffice/controller/HomeController.java
@@ -1,9 +1,18 @@
 package br.com.rede.ke.backoffice.controller;
 
+import br.com.rede.ke.backoffice.conciliation.domain.entity.Acquirer;
 import br.com.rede.ke.backoffice.conciliation.domain.repository.PvRepository;
+import org.hibernate.validator.internal.util.CollectionHelper;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static br.com.rede.ke.backoffice.conciliation.domain.repository.PvSpecifications.*;
+import static org.springframework.data.jpa.domain.Specifications.where;
 
 
 @Controller
@@ -16,8 +25,13 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("pvs", pvRepository.findAll());
+    public String index(Model model,
+                        @PageableDefault Pageable pageable,
+                        @RequestParam(required = false) String code,
+                        @RequestParam(required = false, defaultValue = "REDE") Acquirer acquirer) {
+        model.addAttribute("code", code);
+        model.addAttribute("acquirer", acquirer);
+        model.addAttribute("pvs", pvRepository.findAll(where(pvCodeEqualTo(code)).and(pvAcquirerEqualTo(acquirer)), pageable));
         return "home";
     }
 }
