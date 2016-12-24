b02e6bf3e866888b6aeff1425a4ff63ef5c9eb54
Johnny Richard <jrichard@thoughtworks.com>
2016-12-24 16:44:21 -0200

    Remove .java não usado e adiciona coisas do intellij no gitignore

--- src/main/java/br/com/rede/ke/backoffice/domain/repository/PvRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.rede.ke.backoffice.conciliador.domain.repository;
-
-import br.com.rede.ke.backoffice.conciliador.domain.entity.Pv;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PvRepository extends JpaRepository<Pv, Long> {
-}
